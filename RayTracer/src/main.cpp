925e0416261d273403134e098fe60e5f60b19e2b
THaenlein <40724414+THaenlein@users.noreply.github.com>
2020-05-09 22:37:47 +0200

    Added quick sdl event handling

--- RayTracer/src/main.cpp
+++ RayTracer/src/main.cpp
@@ -23,14 +23,42 @@ int main(int argc, char* argv[])
 		app.setUpSdl();
 
 		texture = app.loadTexture(imagePath);
-		
-		app.render(texture.get());
 	}
 	catch(raytracer::SdlException& exception)
 	{
 		raytracer::ErrorHandler::getInstance().reportError(exception);
 		app.cleanUp();
 		return 0;
+	}
+
+	SDL_Event sdlEvent;
+	bool continueRendering{ true };
+
+	while (continueRendering)
+	{
+		while (SDL_PollEvent(&sdlEvent))
+		{
+			// Do something about this
+			//BOOST_LOG_TRIVIAL(debug) << "Event received:" << sdlEvent.type;
+
+			switch (sdlEvent.type)
+			{
+			case SDL_QUIT:
+				continueRendering = false;
+				break;
+			
+			case SDL_WINDOWEVENT:
+				switch (sdlEvent.window.event)
+				{
+				case SDL_WINDOWEVENT_MOVED:
+					SDL_SetWindowPosition(app.mainWindow.get(), sdlEvent.window.data1, sdlEvent.window.data2);
+					break;
+				}
+				break;
+			}
+		}
+
+		app.render(texture.get());
 	}
 
 	raytracer::ErrorHandler::getInstance().reportInfo("Success. Press Enter to exit SDL...");
