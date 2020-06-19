e0560cfb7a377e4087d19fe452bb84f23ad66dc2
THaenlein <tim_24398@gmx.de>
2020-06-19 03:40:36 +0200

    More tests written for class BoundingBox

--- RayTracer/tests/TestBoundingBox.cpp
+++ RayTracer/tests/TestBoundingBox.cpp
@@ -13,7 +13,9 @@
 //		EXPECT_DOUBLE_EQ
 //		EXPECT_FLOAT_EQ
 
-TEST(BoundingBox, TestIntersects)
+
+// Regular intersection
+TEST(BoundingBox, TestIntersectsRegular)
 {
 	aiVector3D min(1.f, 1.f, 0.f);
 	aiVector3D max(2.f, 2.f, 1.f);
@@ -27,6 +29,52 @@ TEST(BoundingBox, TestIntersects)
 	EXPECT_TRUE(intersects);
 }
 
+// Negative direction vector
+TEST(BoundingBox, TestIntersectsNegative)
+{
+	aiVector3D min(1.f, 1.f, 0.f);
+	aiVector3D max(2.f, 2.f, 1.f);
+	raytracing::BoundingBox box(min, max);
+
+	aiVector3D position(3.f, 3.f, .5f);
+	aiVector3D direction(-1.f, -1.f, 0.f);
+	aiRay ray(position, direction);
+
+	bool intersects = box.intersects(ray);
+	EXPECT_TRUE(intersects);
+}
+
+// Intersects in both directions
+TEST(BoundingBox, TestIntersectsBoth)
+{
+	aiVector3D min(-1.f, -1.f, 0.f);
+	aiVector3D max(1.f, 1.f, 1.f);
+	raytracing::BoundingBox box(min, max);
+
+	aiVector3D position(-2.f, 0.f, .5f);
+	aiVector3D direction(-1.f, 0.f, 0.f);
+	aiRay ray(position, direction);
+
+	bool intersects = box.intersects(ray);
+	EXPECT_TRUE(intersects);
+}
+
+// Line intersection
+TEST(BoundingBox, TestIntersectsLine)
+{
+	aiVector3D min(0.f, 0.f, 0.f);
+	aiVector3D max(1.f, 1.f, 1.f);
+	raytracing::BoundingBox box(min, max);
+
+	aiVector3D position(-1.f, 0.f, .5f);
+	aiVector3D direction(1.f, 0.f, 0.f);
+	aiRay ray(position, direction);
+
+	bool intersects = box.intersects(ray);
+	EXPECT_TRUE(intersects);
+}
+
+
 TEST(BoundingBox, TestNotIntersects)
 {
 	aiVector3D min(1.f, 1.f, 0.f);
@@ -34,9 +82,78 @@ TEST(BoundingBox, TestNotIntersects)
 	raytracing::BoundingBox box(min, max);
 
 	aiVector3D position(0.f, 0.f, .5f);
-	aiVector3D direction(0.f, 0.f, 0.f);
+	aiVector3D direction(0.f, 0.f, 1.f);
 	aiRay ray(position, direction);
 
 	bool doesNotIntersect = !box.intersects(ray);
 	EXPECT_TRUE(doesNotIntersect);
+}
+
+TEST(BoundingBox, TestNotIntersectsNegative)
+{
+	aiVector3D min(1.f, 1.f, 0.f);
+	aiVector3D max(2.f, 2.f, 1.f);
+	raytracing::BoundingBox box(min, max);
+
+	aiVector3D position(3.f, 3.f, .5f);
+	aiVector3D direction(-1.f, 0.f, 0.f);
+	aiRay ray(position, direction);
+
+	bool doesNotIntersect = !box.intersects(ray);
+	EXPECT_TRUE(doesNotIntersect);
+}
+
+
+TEST(BoundingBox, TestSplitX)
+{
+	aiVector3D min(0.f, 0.f, 0.f);
+	aiVector3D max(2.f, 1.f, 1.f);
+	raytracing::BoundingBox box(min, max);
+
+	raytracing::BoundingBox left;
+	raytracing::BoundingBox right;
+
+	box.split(left, right);
+
+	raytracing::BoundingBox expectedLeft(min, aiVector3D(1.f, 1.f, 1.f));
+	EXPECT_EQ(left, expectedLeft);
+
+	raytracing::BoundingBox expectedRight(aiVector3D(1.f, 0.f, 0.f), max);
+	EXPECT_EQ(right, expectedRight);
+}
+
+TEST(BoundingBox, TestSplitY)
+{
+	aiVector3D min(0.f, 0.f, 0.f);
+	aiVector3D max(1.f, 3.f, 1.f);
+	raytracing::BoundingBox box(min, max);
+
+	raytracing::BoundingBox left;
+	raytracing::BoundingBox right;
+
+	box.split(left, right);
+
+	raytracing::BoundingBox expectedLeft(min, aiVector3D(1.f, 1.5f, 1.f));
+	EXPECT_EQ(left, expectedLeft);
+
+	raytracing::BoundingBox expectedRight(aiVector3D(0.f, 1.5f, 0.f), max);
+	EXPECT_EQ(right, expectedRight);
+}
+
+TEST(BoundingBox, TestSplitZ)
+{
+	aiVector3D min(0.f, 0.f, 0.f);
+	aiVector3D max(1.f, 1.f, 4.f);
+	raytracing::BoundingBox box(min, max);
+
+	raytracing::BoundingBox left;
+	raytracing::BoundingBox right;
+
+	box.split(left, right);
+
+	raytracing::BoundingBox expectedLeft(min, aiVector3D(1.f, 1.f, 2.f));
+	EXPECT_EQ(left, expectedLeft);
+
+	raytracing::BoundingBox expectedRight(aiVector3D(0.f, 0.f, 2.f), max);
+	EXPECT_EQ(right, expectedRight);
 }
