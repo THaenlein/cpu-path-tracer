56a3ceaebc4982edb54db478f678f777381bf211
Tim Haenlein <tim_24398@gmx.de>
2021-04-23 08:13:12 +0200

    Removed outcommented calculation for intersection/ traversal cost

--- PathTracer/src/main.cpp
+++ PathTracer/src/main.cpp
@@ -363,50 +363,5 @@ int main(int argc, char* argv[])
 	
 	assetImporter.FreeScene();
 
-	// Calculate Intersection/Traversal cost
-
-	//std::vector<std::vector<aiVector3D*>> triangles;
-	//std::vector<raytracing::BoundingBox> bBoxes;
-	//for (raytracing::KdTriangle tri : triangleMeshCollection)
-	//{
-	//	aiFace* currentFace{ tri.faceMeshPair.first };
-	//	aiMesh* associatedMesh{ tri.faceMeshPair.second };
-	//	std::vector<aiVector3D*> faceTris;
-	//	for (unsigned int currentIndex = 0; currentIndex < currentFace->mNumIndices; currentIndex++)
-	//	{
-	//		faceTris.push_back(&(associatedMesh->mVertices[currentFace->mIndices[currentIndex]]));
-	//	}
-	//	triangles.push_back(faceTris);
-	//	bBoxes.push_back(raytracing::BoundingBox(tri));
-	//}
-	//
-	//aiRay someRay({ 7.1867f, 0.f, 1.66125f }, {-0.939693f, 0.f, -0.34202f});
-	//aiVector3D tempIs;
-	//aiVector2D tempUv;
-	//
-	//raytracing::Timer::getInstance().start();
-	//for (std::vector<aiVector3D*>& tri : triangles)
-	//{
-	//	utility::mathUtility::rayTriangleIntersection(someRay, tri, &tempIs, &tempUv);
-	//
-	//}
-	//double intersectionTime = raytracing::Timer::getInstance().stop();
-	//double averageIsTime = intersectionTime / static_cast<double>(triangles.size());
-	//
-	//raytracing::Timer::getInstance().start();
-	//for (raytracing::BoundingBox& box : bBoxes)
-	//{
-	//	box.intersects(someRay);
-	//}
-	//double travTime = raytracing::Timer::getInstance().stop();
-	//double averageTravTime = travTime / static_cast<double>(bBoxes.size());
-	//
-	//
-	//SDL_Log("Intersection time: %f seconds", intersectionTime);
-	//SDL_Log("Average intersection time: %f seconds", averageIsTime);
-	//SDL_Log("Average trav time: %f seconds", averageTravTime);
-	//SDL_Log("Traversal time: %f seconds", travTime);
-	//std::cin.get();
-
 	return 0;
 }
